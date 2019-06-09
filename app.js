40b0cc610ccc19789f9a9fd478b267ace8158f53
Andrew <andydrew313@gmail.com>
2019-06-09 15:14:12 -0700

    multilayer nav

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var fellowshipRouter = require('./routes/fellowships');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(favicon(path.join(__dirname, 'public', 'images', 'icon.ico')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', fellowshipRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
